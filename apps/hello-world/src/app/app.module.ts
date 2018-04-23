b0c869d8dbe394ed9f76deda7ac72f97cafcedff
Emilio Martinez <e.marticor@gmail.com>
2018-04-23 11:04:39 -0700

    fix(hello-world): allow for both browser and worker flows

--- apps/hello-world/src/app/app.module.ts
+++ apps/hello-world/src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-// import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { WorkerAppModule } from '@angular/platform-webworker';
 
 @NgModule({
-  imports: [WorkerAppModule],
   declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  exports: [AppComponent]
 })
 export class AppModule {}
+
+@NgModule({
+  imports: [AppModule, BrowserModule],
+  bootstrap: [AppComponent]
+})
+export class AppBrowserModule {}
+
+@NgModule({
+  imports: [AppModule, WorkerAppModule],
+  bootstrap: [AppComponent]
+})
+export class AppWorkerModule {}
